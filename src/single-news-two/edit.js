6022bfe2e0eb4abf7be6816f221630a1cefb802f
kishan007 <hashthemesu4@gmail.com>
2023-04-12 14:58:58 +0545

    updated

--- src/single-news-two/edit.js
+++ src/single-news-two/edit.js
@@ -547,6 +547,23 @@ export default function Edit({ attributes, setAttributes }) {
                             <GradientPicker
                                 value={ contentOverlayBackground }
                                 onChange={ ( contentOverlayBackground ) => setAttributes( {contentOverlayBackground} ) }
+                                gradients={ [
+                                    {
+                                        name: 'JShine',
+                                        gradient: 'linear-gradient(135deg,#12c2e9 0%,#c471ed 50%,#f64f59 100%)',
+                                        slug: 'jshine',
+                                    },
+                                    {
+                                        name: 'Moonlit Asteroid',
+                                        gradient: 'linear-gradient(135deg,#0F2027 0%, #203A43 0%, #2c5364 100%)',
+                                        slug: 'moonlit-asteroid',
+                                    },
+                                    {
+                                        name: 'Rastafarie',
+                                        gradient: 'linear-gradient(135deg,#1E9600 0%, #FFF200 0%, #FF0000 100%)',
+                                        slug: 'rastafari',
+                                    },
+                                ] }
                             />
                             <Dimension
                                 label={ __('Content Padding', 'smart-blocks') }
