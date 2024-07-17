3ff158cd8375f11ba9f2b0380dbc69621d2e0dce
Prabin Dangol <neprabin@gmail.com>
2024-07-17 18:08:19 +0545

    Update helper.js

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -30,18 +30,18 @@ const dimensionVars = (varname, top, right, bottom, left, unit) => {
 }
 
 const responsiveDimensionVars = (varname, top, right, bottom, left, topSm, rightSm, bottomSm, leftSm, topMd, rightMd, bottomMd, leftMd, unit) => {
-    return `--sb-${varname}-top-sm: ${topSm ? (topSm + unit) : ''};
-        --sb-${varname}-right-sm: ${rightSm ? (rightSm + unit) : ''};
-        --sb-${varname}-bottom-sm: ${bottomSm ? (bottomSm + unit) : ''};
-        --sb-${varname}-left-sm: ${leftSm ? (leftSm + unit): ''};
-        --sb-${varname}-top-md: ${topMd ? (topMd + unit) : ''};
-        --sb-${varname}-right-md: ${rightMd ? (rightMd + unit) : ''};
-        --sb-${varname}-bottom-md: ${bottomMd ? (bottomMd + unit) : ''};
-        --sb-${varname}-left-md: ${leftMd ? (leftMd + unit) : ''};
-        --sb-${varname}-top-lg: ${top ? (top + unit) : ''};
-        --sb-${varname}-right-lg: ${right ? (right + unit) : ''};
-        --sb-${varname}-bottom-lg: ${bottom ? (bottom + unit) : ''};
-        --sb-${varname}-left-lg: ${left ? (left + unit) : ''};`;
+    return `--sb-${varname}-top-sm: ${topSm ? (topSm + unit) : 'var(--sb-' + varname + '-top-md)'};
+        --sb-${varname}-right-sm: ${rightSm ? (rightSm + unit) : 'var(--sb-' + varname + '-right-md)'};
+        --sb-${varname}-bottom-sm: ${bottomSm ? (bottomSm + unit) : 'var(--sb-' + varname + '-bottom-md)'};
+        --sb-${varname}-left-sm: ${leftSm ? (leftSm + unit): 'var(--sb-' + varname + '-left-md)'};
+        --sb-${varname}-top-md: ${topMd ? (topMd + unit) : 'var(--sb-' + varname + '-top-lg)'};
+        --sb-${varname}-right-md: ${rightMd ? (rightMd + unit) : 'var(--sb-' + varname + '-right-lg)'};
+        --sb-${varname}-bottom-md: ${bottomMd ? (bottomMd + unit) : 'var(--sb-' + varname + '-bottom-lg)'};
+        --sb-${varname}-left-md: ${leftMd ? (leftMd + unit) : 'var(--sb-' + varname + '-left-lg)'};
+        --sb-${varname}-top-lg: ${top ? (top + unit) : 'initial'};
+        --sb-${varname}-right-lg: ${right ? (right + unit) : 'initial'};
+        --sb-${varname}-bottom-lg: ${bottom ? (bottom + unit) : 'initial'};
+        --sb-${varname}-left-lg: ${left ? (left + unit) : 'initial'};`;
 }
 
 export { checkDefault, getFontClass, responsiveDimensionVars, dimensionVars };
